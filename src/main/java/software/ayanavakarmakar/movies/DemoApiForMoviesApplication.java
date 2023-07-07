d1fe00588e3b2d01b6de2cd8f07bdff76290f31f
Ayanava Karmakar <ayanava@rapidken.ai>
2023-07-08 02:27:34 +0530

    chore: init spring boot project

---
+++ src/main/java/software/ayanavakarmakar/movies/DemoApiForMoviesApplication.java
@@ -0,0 +1,20 @@
+package software.ayanavakarmakar.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApiForMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiForMoviesApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiRoot() {
+		return "Hello, World";
+	}
+}
---
+++ src/test/java/software/ayanavakarmakar/movies/DemoApiForMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package software.ayanavakarmakar.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiForMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
