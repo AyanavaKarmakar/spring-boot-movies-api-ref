c5b230e0470195003ff4ff56d365c35a2576ce04
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-07-08 16:01:01 +0530

    task: get all movies list from DB

--- src/main/java/software/ayanavakarmakar/movies/controllers/MovieController.java
+++ src/main/java/software/ayanavakarmakar/movies/controllers/MovieController.java
@@ -1,4 +1,26 @@
 package software.ayanavakarmakar.movies.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import software.ayanavakarmakar.movies.models.Movie;
+import software.ayanavakarmakar.movies.services.MovieService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/movies")
 public class MovieController {
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
+    }
 }
